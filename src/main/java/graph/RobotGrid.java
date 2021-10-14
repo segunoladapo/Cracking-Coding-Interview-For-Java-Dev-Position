282c5b15620ee6554b35760dbd80d69dd7e9a4ee
Segun Oladapo <segun.holadapo@gmail.com>
2021-10-14 11:42:43 -0500

    Add RobotGrid recursive implementation (2^M+N) Can be improved using memoization or tabulation

---
+++ src/main/java/graph/RobotGrid.java
@@ -0,0 +1,43 @@
+package graph;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RobotGrid {
+
+    public static boolean computePath(int m, int n, boolean[][] maze, List<Point> paths) {
+        System.out.println("Entering COmputerPath...");
+        if (m < 0 || n < 0) {
+            return false;
+        }
+
+        if (!maze[m][n]) {
+            return false;
+        }
+        if ((m == 0 && n == 0) || computePath(m - 1, n, maze, paths) || computePath(m, n - 1, maze, paths)) {
+            paths.add(new Point(m, n));
+            return true;
+        }
+        return false;
+    }
+
+
+    public static void main(String... args) {
+        List<Point> paths = new ArrayList<>();
+        boolean[][] maze = new boolean[6][6];
+        maze[0][0] = true;
+        maze[5][5] = true;
+        maze[4][5] = true;
+        maze[4][4] = true;
+        maze[3][4] = true;
+        maze[3][3] = true;
+        maze[3][2] = true;
+        maze[3][1] = true;
+        maze[2][1] = true;
+        maze[1][1] = true;
+        maze[0][1] = true;
+        computePath(5, 5, maze, paths);
+        System.out.println(paths);
+    }
+}
